1bb25117c1dba6bfeffa4dd3802108b3aa49a23c
Eduard "Menza" Hopfer <core@ddcveng.cc>
2022-02-08 20:25:23 +0100

    pop os version backup

--- config.def.h
+++ config.def.h
@@ -66,7 +66,7 @@ static const Rule rules[] = {
 /* window swallowing */
 static const int swaldecay = 3;
 static const int swalretroactive = 1;
-static const char swalsymbol[] = "👅";
+static const char swalsymbol[] = ":p";
 
 /* layout(s) */
 static const float mfact = 0.50; /* factor of master area size [0.05..0.95] */
